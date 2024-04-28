71c4a53602489c679bbd49570aafe7f8cd4d709b
Lucas Legrand <legrand.lucas0@gmail.com>
2024-04-28 17:32:40 +0200

    subscriptions

--- ytbtools/src/App.js
+++ ytbtools/src/App.js
@@ -1,37 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import {axios} from 'axios'
-import React, {useEffect} from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
 
 function App() {
+  const [datas, setDatas] = useState([])
 
 
 
 const getSubscriptions = () => {
-  axios.get('https://youtube.googleapis.com/youtube/v3/subscriptions?part=contentDetails&channelId=UCIJG2skTIeZzd7Cb3YZ7JiA&key=AIzaSyAMGSKwDt3p0TTv4_3cUtpEKuJtS5Ua-uU').then((res) => res.json())
-  .then((data) => data.items.forEach((curr) => console.log(curr)))
+  axios.get('https://youtube.googleapis.com/youtube/v3/subscriptions?part=contentDetails&channelId=UCIJG2skTIeZzd7Cb3YZ7JiA&key=AIzaSyAMGSKwDt3p0TTv4_3cUtpEKuJtS5Ua-uU')
+  .then((res) =>  setDatas(res.data.items))
 }
 
 useEffect(() => {
   getSubscriptions()
 }, [])
 
+console.log(datas)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {datas.map((item) => <p style={{padding: "20px"}}>
+      {JSON.stringify(item)}
+    </p>)}
     </div>
   );
 }
--- ytbtools/src/actions/youtubeApi.js
+++
@@ -1,6 +0,0 @@
-
-
-const getSubscriptions = () => {
-  axios.get('https://youtube.googleapis.com/youtube/v3/subscriptions?part=contentDetails&channelId=UCIJG2skTIeZzd7Cb3YZ7JiA&key=AIzaSyAMGSKwDt3p0TTv4_3cUtpEKuJtS5Ua-uU').then((res) => res.json())
-  .then((data) => data.items.forEach((curr) => console.log(curr)))
-}
